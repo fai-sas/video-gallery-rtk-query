61d0e992ba53520a5490c07e6b4835598eb38a02
fai-sas <craftedbyfaisal@gmail.com>
2024-01-15 20:13:00 +0600

    videos fetched and rendered

--- src/components/videos/Video.jsx
+++ src/components/videos/Video.jsx
@@ -1,21 +1,25 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 import { Link } from 'react-router-dom'
 import authorImage from '../../assets/author.png'
 
-export default function Video() {
+export default function Video({ video }) {
+  console.log(video)
+  const { id, title, duration, thumbnail, author, views, date } = video
   return (
     <div className='col-span-12  sm:col-span-6 md:col-span-3 duration-300 hover:scale-[1.03]'>
       <div className='flex flex-col w-full'>
         <div className='relative'>
-          <Link to='/videos/1'>
+          <Link to={`/videos/${id}`}>
             <img
-              src='https://i3.ytimg.com/vi/6O4s7v28nlw/maxresdefault.jpg'
+              src={thumbnail}
               className='w-full h-auto rounded-md'
-              alt='Some video title'
+              alt={title}
             />
           </Link>
 
           <p className='absolute px-1 text-xs text-gray-100 bg-gray-900 right-2 bottom-2 py'>
-            12:10
+            {duration}
           </p>
         </div>
 
@@ -23,19 +27,19 @@ export default function Video() {
           <img
             src={authorImage}
             className='w-6 h-6 rounded-full shrink-0'
-            alt='Learn with Sumit'
+            alt={author}
           />
 
           <div className='flex flex-col'>
             <Link to='/videos/1'>
-              <p className='text-sm font-semibold text-slate-900'>
-                Video title
-              </p>
+              <p className='text-sm font-semibold text-slate-900'>{title}</p>
             </Link>
             <span className='text-xs text-gray-400 hover:text-gray-600'>
-              Learn with Sumit
+              {author}
             </span>
-            <p className='text-xs text-gray-400'>200 views . May 3, 2022</p>
+            <p className='text-xs text-gray-400'>
+              {views} views . {date}
+            </p>
           </div>
         </div>
       </div>
--- src/components/videos/Videos.jsx
+++ src/components/videos/Videos.jsx
@@ -1,15 +1,31 @@
-import Video from "./Video";
+/* eslint-disable no-unused-vars */
+import { useGetVideosQuery } from '../../features/api/apiSlice'
+import Video from './Video'
+import VideoLoader from '../../components/ui/loaders/VideoLoader'
+import Error from '../../components/ui/Error'
 
 export default function Videos() {
-    return (
-        <>
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-        </>
-    );
+  const { data: videos, isLoading, isError } = useGetVideosQuery()
+
+  let content = null
+
+  if (isLoading) {
+    content = <VideoLoader />
+  }
+
+  if (!isLoading && isError) {
+    content = <Error message='There was an error' />
+  }
+
+  if (!isLoading && !isError && videos?.length === 0) {
+    content = <Error message='No videos found' />
+  }
+
+  if (!isLoading && !isError && videos.length > 0) {
+    content = videos?.map((video) => {
+      return <Video key={video.id} video={video} />
+    })
+  }
+
+  return content
 }

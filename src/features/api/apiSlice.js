1b837a1478a309e885c33c7fdf08f674409df141
fai-sas <craftedbyfaisal@gmail.com>
2024-01-15 19:48:29 +0600

    store and apiSlice created

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -4,5 +4,11 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:9000' }),
-  endpoints: (builder) => ({}),
+  endpoints: (builder) => ({
+    getVideos: builder.query({
+      query: () => '/videos',
+    }),
+  }),
 })
+
+export const { useGetVideosQuery } = apiSlice
